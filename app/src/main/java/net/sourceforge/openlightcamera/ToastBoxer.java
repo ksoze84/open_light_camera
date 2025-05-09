f9c47c57b612f25c57b6b1615bdb796ec933d682
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2025-05-09 14:57:30 -0400

    inicial

---
+++ app/src/main/java/net/sourceforge/openlightcamera/ToastBoxer.java
@@ -0,0 +1,12 @@
+package net.sourceforge.openlightcamera;
+
+import android.widget.Toast;
+
+/** Allows methods to update a Toast with a new Toast.
+ */
+public class ToastBoxer {
+    public Toast toast;
+
+    public ToastBoxer() {
+    }
+}
